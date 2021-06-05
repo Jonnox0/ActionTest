b2d0fc9953c44e2f359885cc626864c3303f1785
Jonnox0 <der.jan.kuehling@gmx.de>
2021-06-05 04:02:58 +0200

    first commit

---
+++ ActionTest/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace ActionTest
+{
+  using System;
+
+  /// <summary>
+  /// The program.
+  /// </summary>
+  class Program
+  {
+    /// <summary>
+    /// The main.
+    /// </summary>
+    /// <param name="args">
+    /// The args.
+    /// </param>
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hallol World!");
+    }
+  }
+}
