6bffa8377adf1bcdcbb21f93ab65c5802552218f
Jonnox0 <der.jan.kuehling@gmx.de>
2021-07-11 15:41:51 +0200

    Added some example Tests

---
+++ TestWebApplication.Test/Class1.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace TestWebApplication.Test
+{
+  using System;
+  using Xunit;
+
+  public class Class1
+  {
+    [Fact]
+    public void Error()
+    {
+      throw new Exception();
+    }
+
+    [Fact]
+    public void Ok()
+    {
+      return;
+    }
+  }
+}
